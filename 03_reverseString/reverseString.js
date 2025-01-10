01a64aa1dafd0b2bd5c288c9d5eda257b31f3dc8
Dayrielakhier <176357699+Dayrielakhier@users.noreply.github.com>
2025-01-10 14:14:16 +0800

    Add appropriate function to pass the test

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    splitted = string.split("");
+    array = splitted.reverse();
+    result = array.join("");
+    return result;
 };
 
 // Do not edit below this line
