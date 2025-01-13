342f00aa8117aef54dacb8b89a8deac72ac9c746
Dayrielakhier <176357699+Dayrielakhier@users.noreply.github.com>
2025-01-13 15:48:36 +0800

    Add func to sum positive integers

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    let finalSum = +"";
+    if (num1 < 0 || num1 % 1 !== 0 || typeof num1 !== "number" ||
+        num2 < 0 || num2 % 1 !== 0 || typeof num2 !== "number") {
+            return 'ERROR'
+        } else if (num1 < num2) {
+        for (let i = num1; i <= num2; i++) {
+            finalSum += i;
+            }
+        } else {
+            for (let i = num1; i >= num2; i--) {
+                finalSum += i;
+                }
+        };
+    
+    return finalSum
 };
 
 // Do not edit below this line
