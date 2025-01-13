38b3163a029f9a310be5c337157deea97e1e04a6
Dayrielakhier <176357699+Dayrielakhier@users.noreply.github.com>
2025-01-13 16:59:34 +0800

    Add 2 functions to convert temperatures

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const rounded = number => Math.round(number * 10)/10;
+
+const convertToCelsius = function(degree) {
+  converted = (degree - 32) * (5/9);
+  return rounded(converted)
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(degree) {
+  converted = degree * (9/5) + 32;
+  return rounded(converted)
 };
 
 // Do not edit below this line
