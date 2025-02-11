defa62f7187e0af495e17c523d5c934a14d07c9b
Dayrielakhier <176357699+Dayrielakhier@users.noreply.github.com>
2025-02-11 16:50:33 +0800

    Declare a variable to be more readable

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,8 +1,9 @@
 const palindromes = function (string) {
     let lowerCased = string.toLowerCase()
-    let charOnly = lowerCased.replace(/[^\w]|/g, "")
+    let cleanedString = lowerCased.replace(/[^\w]|/g, "")
+    let reversedString = cleanedString.split('').reverse().join('')
     
-    return charOnly === charOnly.split('').reverse().join('')
+    return cleanedString === reversedString
 };
 
 // Do not edit below this line
