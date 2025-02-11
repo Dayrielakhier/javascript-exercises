b62fb9144d7500af6927b841d86ddc29f1ce7e92
Dayrielakhier <176357699+Dayrielakhier@users.noreply.github.com>
2025-02-11 16:46:17 +0800

    Add function according to the spec

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let lowerCased = string.toLowerCase()
+    let charOnly = lowerCased.replace(/[^\w]|/g, "")
+    
+    return charOnly === charOnly.split('').reverse().join('')
 };
 
 // Do not edit below this line
