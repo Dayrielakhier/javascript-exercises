e05c31a5acd38c3396653e381deac99e6e2df514
Dayrielakhier <176357699+Dayrielakhier@users.noreply.github.com>
2025-02-12 15:49:56 +0800

    Add function according to the specs

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    const currentYear = new Date().getFullYear()
+    arr.sort((a, b) => {
+        if (!a.yearOfDeath || !b.yearOfDeath) {
+            a.yearLived = currentYear - a.yearOfBirth
+            b.yearLived = currentYear - b.yearOfBirth
+        } else {
+            a.yearLived = a.yearOfDeath - a.yearOfBirth
+            b.yearLived = b.yearOfDeath - b.yearOfBirth
+        }
+        return b.yearLived - a.yearLived
+    })
+    return arr[0]
 };
 
 // Do not edit below this line
