b315509923d1d6d394583e904d17c219488400ee
Dayrielakhier <176357699+Dayrielakhier@users.noreply.github.com>
2025-02-12 15:12:57 +0800

    Add function according to the specs

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
-
+const fibonacci = function(x) {
+    const num = Number(x)
+    if (num < 0) {
+        return "OOPS"
+    } else if (num === 0) {
+        return 0
+    } else {
+        const fibonacci = []
+        for (let i = 0; i < num; i++) {
+            if (i === 0 || i === 1) {
+                let n = 1
+                fibonacci.push(n)
+            } else {
+                let n = fibonacci[(i - 1)] + fibonacci[(i - 2)]
+                fibonacci.push(n)
+            }
+        }
+        return fibonacci.at(-1)
+    }
 };
 
 // Do not edit below this line
